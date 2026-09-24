14f8c4d9d764cb34593dbabbfdf3a23423860da5
Erenshoyo <asiftauhid4665@gmail.com>
2026-09-25 01:03:23 +0600

    Feat: Worked on authController

--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -6,65 +6,133 @@ import type { IRequestUser } from "./auth.interface";
 import { AuthService } from "./auth.service";
 
 const setAuthCookies = (
-	res: Response,
-	accessToken: string,
-	refreshToken: string,
+  res: Response,
+  accessToken: string,
+  refreshToken: string,
 ) => {
-	const secure = process.env.NODE_ENV === "production";
-	res.cookie("accessToken", accessToken, {
-		httpOnly: true,
-		secure,
-		sameSite: secure ? "none" : "lax",
-		maxAge: 86400000,
-	});
-	res.cookie("refreshToken", refreshToken, {
-		httpOnly: true,
-		secure,
-		sameSite: secure ? "none" : "lax",
-		maxAge: 604800000,
-	});
+  const secure = process.env.NODE_ENV === "production";
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    secure,
+    sameSite: secure ? "none" : "lax",
+    maxAge: 86400000,
+  });
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure,
+    sameSite: secure ? "none" : "lax",
+    maxAge: 604800000,
+  });
 };
 
 const registerUser = catchAsync(async (req: Request, res: Response) => {
-	const result = await AuthService.registerUser(req.body);
-	sendResponse(res, {
-		statusCode: httpStatus.CREATED,
-		success: true,
-		message: "Registration started.",
-		data: result,
-	});
+  const result = await AuthService.registerUser(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: "Verification OTP sent to your email.",
+    data: result,
+  });
 });
 
 const verifyEmail = catchAsync(async (req: Request, res: Response) => {
-	const result = await AuthService.verifyEmail(req.body);
-	setAuthCookies(res, result.accessToken, result.refreshToken);
-	sendResponse(res, {
-		statusCode: httpStatus.CREATED,
-		success: true,
-		message: "Email verified successfully.",
-		data: result,
-	});
+  const result = await AuthService.verifyEmail(req.body);
+  setAuthCookies(res, result.accessToken, result.refreshToken);
+  sendResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: "Email verified successfully.",
+    data: result,
+  });
 });
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
-	const result = await AuthService.loginUser(req.body);
-	setAuthCookies(res, result.accessToken, result.refreshToken);
-	sendResponse(res, {
-		statusCode: httpStatus.OK,
-		success: true,
-		message: "User logged in successfully.",
-		data: result,
-	});
+  const result = await AuthService.loginUser(req.body);
+  setAuthCookies(res, result.accessToken, result.refreshToken);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "User logged in successfully.",
+    data: result,
+  });
 });
 
 const getMe = catchAsync(async (req: Request, res: Response) => {
-	const result = await AuthService.getMe(req.user as IRequestUser);
-	sendResponse(res, {
-		statusCode: httpStatus.OK,
-		success: true,
-		message: "User profile fetched successfully.",
-		data: result,
-	});
+  const result = await AuthService.getMe(req.user as IRequestUser);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "User profile fetched successfully.",
+    data: result,
+  });
 });
 
-export const AuthController = { registerUser, verifyEmail, loginUser, getMe };
+const refreshToken = catchAsync(async (req: Request, res: Response) => {
+  if (!req.cookies.refreshToken) {
+    throw new Error("Refresh token is missing");
+  }
+  const result = await AuthService.refreshToken(req.cookies.refreshToken);
+  const { accessToken, refreshToken: newRefreshToken } = result;
+
+  setAuthCookies(res, result.accessToken, result.refreshToken);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "New tokens generated successfully",
+    data: {
+      accessToken,
+      refreshToken: newRefreshToken,
+    },
+  });
+});
+
+const googleLogin = catchAsync(async (req: Request, res: Response) => {
+  const payload = req.body;
+  const result = await AuthService.googleLogin(payload);
+
+  const { accessToken, refreshToken } = result;
+
+  setAuthCookies(res, result.accessToken, result.refreshToken);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Google login successful",
+    data: { accessToken, refreshToken },
+  });
+});
+
+const forgotPassword = catchAsync(async (req: Request, res: Response) => {
+  const payload = req.body;
+  await AuthService.forgotPassword(payload);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: `OTP sent To Email: ${payload.email}`,
+    data: null,
+  });
+});
+
+const resetPassword = catchAsync(async (req: Request, res: Response) => {
+  const payload = req.body;
+  await AuthService.resetPassword(payload);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Password Changed Successfully",
+    data: null,
+  });
+});
+
+export const AuthController = {
+  registerUser,
+  verifyEmail,
+  loginUser,
+  getMe,
+  refreshToken,
+  googleLogin,
+  forgotPassword,
+  resetPassword,
+};
